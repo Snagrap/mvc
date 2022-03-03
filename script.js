2afe8875e8c02aa80be550c755e5cb2194857344
Snagrap <karlkarilaid04@gmail.com>
2022-03-03 17:33:07 +0200

    Created project basic view

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 class Model {
-	constructor() {}
+	constructor() {
+		this.tasks = [
+		{id:1, text: 'Be good', complete: false},
+		{id:2, text: 'Be nice', complete: false}
+		]
+	}
 }
 
 class View {
-	constructor() {}
+	constructor() {
+		this.app = this.getElement('#root')
+		this.title = this.setElement('h1')
+		this.title.textContent = 'Tasks'
+		this.taskList = this.setElement('ul')
+		this.app.append(this.title, this.taskList)
+	}
+
+	getElement(selector){
+		const element = document.querySelector(selector)
+		return element
+	}
+
+	setElement(tag, classname){
+		const element = document.createElement(tag)
+		if(classname !== undefined){
+			element.classList.add(classname)
+		}
+		return element
+	}
 }
 
 class Controller {
